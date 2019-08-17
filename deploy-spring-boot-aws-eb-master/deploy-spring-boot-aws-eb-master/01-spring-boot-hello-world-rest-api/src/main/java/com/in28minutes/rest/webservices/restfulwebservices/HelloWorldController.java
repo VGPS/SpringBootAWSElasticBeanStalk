b1aefc92ed0e4b627f4372ff50b2fd4ca784d532
VGPS <45807150+VGPS@users.noreply.github.com>
2019-08-17 09:02:05 -0500

    edited with URLS

--- deploy-spring-boot-aws-eb-master/deploy-spring-boot-aws-eb-master/01-spring-boot-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ deploy-spring-boot-aws-eb-master/deploy-spring-boot-aws-eb-master/01-spring-boot-hello-world-rest-api/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -7,18 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+	// http://localhost:5000/hello-world
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
 	}
-
+	
+	// http://localhost:5000/hello-world-bean
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		//throw new RuntimeException("Some Error has Happened! Contact Support at ***-***");
 		return new HelloWorldBean("Hello World - Changed");
 	}
 	
-	///hello-world/path-variable/in28minutes
+	//  http://localhost:5000/hello-world/path-variable/in28minutes
+	//  hello-world/path-variable/in28minutes
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
